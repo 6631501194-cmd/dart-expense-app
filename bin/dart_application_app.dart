4884e20eb69a723d4ef4dc225a26c827045f1b57
Zaw Linn Phyo <6631501194@lamduan.mfu.ac.th>
2025-08-24 20:58:49 +0700

    initial commit

---
+++ bin/dart_application_app.dart
@@ -0,0 +1,5 @@
+import 'package:dart_application_app/dart_application_app.dart' as dart_application_app;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_application_app.calculate()}!');
+}
---
+++ test/dart_application_app_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_application_app/dart_application_app.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
